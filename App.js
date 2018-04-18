99e1c5806bc36a4ca07e2a84c0b8b8ce51c2d22b
Moussa Sarr <moussasarr@moussas-mbp.home>
2018-04-17 23:43:56 -0400

    Navigation screens boilerplate and slides done

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import WelcomeScreen from './screens/WelcomeScreen';
+import DeckScreen from './screens/DeckScreen';
+import MapScreen from './screens/MapScreen';
+import AuthScreen from './screens/AuthScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: { screen: StackNavigator({
+            review: { screen: ReviewScreen },
+            Settings: { screen: SettingsScreen }
+          })}
+        })
+      }
+    });
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center'
+  },
+});
---
+++ components/Slides.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class Slides extends Component {
+
+  renderLastSlide(index) {
+    if( index === this.props.data.length - 1 ) {
+      return ( <Button
+        buttonStyle={styles.button}
+        title="Onwards!"
+        onPress={() => this.props.onComplete}
+        />
+      );
+    }
+  }
+  renderSlides() {
+    return this.props.data.map((slide, index) => {
+      return (
+        <View key={slide.text} style={[styles.slide, { backgroundColor: slide.color}]}>
+          <Text style={styles.slideText}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
+        </View>
+      );
+    })
+  }
+
+  render() {
+    return (
+      <ScrollView
+        horizontal
+        pagingEnabled
+        style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
+    )
+  }
+}
+
+const styles = {
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH
+  },
+  slideText: {
+    fontSize: 36,
+    color: 'white'
+  },
+  button: {
+    backgroundColor: '#0288D1',
+    marginTop: 15
+  }
+}
+
+export default Slides;
